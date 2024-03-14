00926c4edc2dede272b45ac160405bc5f9d2ef7d
Simeon Georgiev <simeongeorgiev09@gmail.com>
2024-03-14 17:17:48 +0200

    Finish newspaper page-only tree build

--- newspaper2tree/src/build_newspaper_tree.py
+++ newspaper2tree/src/build_newspaper_tree.py
@@ -1,6 +1,11 @@
 import re
 
-prefix_regexes = list(enumerate([r'^§ \d+\. ']))
+prefix_regexes = [
+    (0, r'^§ \d+\. '),
+    (1, r'^\d+\. '),
+    (2, r'^"?\w\) '),
+    (3, r'^"?\(\d+\) '),
+]
 
 def find_newspaper_metadata(newspaper_lines):
     for line_idx, line in enumerate(newspaper_lines):
@@ -8,20 +13,27 @@ def find_newspaper_metadata(newspaper_lines):
            return line_idx
 
 
-def find_line_prefix(line):
-    for reg_idx, prefix_reg in prefix_regexes:
+def find_line_prefix_weight(line):
+    for reg_weight, prefix_reg in prefix_regexes:
         if re.search(prefix_reg, line):
-            return reg_idx
+            return reg_weight 
 
-def section_split(content):
+def prefix_lines(lines):
+    nonempty_lines = [ line for line in lines if line.strip() != '' ]
+    content_lines = [ {'idx': line_idx, 'line':line, 'weight':find_line_prefix_weight(line)} for line_idx, line in enumerate(nonempty_lines)]
 
-    section_prefix_reg_idx = find_line_prefix(content)
+    return content_lines
 
-    sections = re.split(prefix_regexes[section_prefix_reg_idx][1], content, flags=re.MULTILINE)
+def find_parent_idx(content_lines, child_line):
 
-    return sections
+    line_idx = child_line['idx']
+    child_prefix = child_line['weight']
 
-    
+    while line_idx >= 0:
+        if content_lines[line_idx]['weight'] < child_prefix:
+            return line_idx
+        line_idx-=1 
+
 
 def build_newspaper_tree(newspaper):
 
@@ -30,6 +42,17 @@ def build_newspaper_tree(newspaper):
     newspaper_info_starts_at = find_newspaper_metadata(newspaper_lines)
     assert newspaper_info_starts_at is not None
     
-    newspaper_content = ''.join(newspaper_lines[newspaper_info_starts_at:])
-    sections = section_split(newspaper_content)
+    #TODO find law from metadata. law will be tree root
 
+    content_lines = prefix_lines(newspaper_lines[newspaper_info_starts_at:])
+    for content_line in content_lines:
+        content_line['parent'] = find_parent_idx(content_lines, content_line)
+
+    newspaper_tree = {
+        'law': [ content_line for content_line in content_lines if content_line['parent'] is None  ]
+    }
+
+    for content_line in content_lines:
+        newspaper_tree[content_line['line']] = [ child_line for child_line in content_lines if child_line['parent'] == content_line['idx'] ]
+
+    return newspaper_tree
