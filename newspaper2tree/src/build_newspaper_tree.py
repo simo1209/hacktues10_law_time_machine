c912f19c2fcac56eceeeb292bc7457192c7c1ac6
Simeon Georgiev <simeongeorgiev09@gmail.com>
2024-03-14 15:36:48 +0200

    add section split

--- newspaper2tree/src/build_newspaper_tree.py
+++ newspaper2tree/src/build_newspaper_tree.py
@@ -1,13 +1,35 @@
 import re
 
-prefix_regexes = [r'^§ \d+\.(.*)']
+prefix_regexes = list(enumerate([r'^§ \d+\. ']))
 
 def find_newspaper_metadata(newspaper_lines):
     for line_idx, line in enumerate(newspaper_lines):
-        if any(re.search(prefix_re, line) for prefix_re in prefix_regexes):
+        if any(re.search(prefix_reg, line) for _, prefix_reg in prefix_regexes):
            return line_idx
 
-def build_newspaper_tree(newspaper):
-    pass
+
+def find_line_prefix(line):
+    for reg_idx, prefix_reg in prefix_regexes:
+        if re.search(prefix_reg, line):
+            return reg_idx
+
+def section_split(content):
+
+    section_prefix_reg_idx = find_line_prefix(content)
+
+    sections = re.split(prefix_regexes[section_prefix_reg_idx][1], content, flags=re.MULTILINE)
+
+    return sections
+
     
+
+def build_newspaper_tree(newspaper):
+
+    newspaper_lines = newspaper.split('\n')
+
+    newspaper_info_starts_at = find_newspaper_metadata(newspaper_lines)
+    assert newspaper_info_starts_at is not None
+    
+    newspaper_content = ''.join(newspaper_lines[newspaper_info_starts_at:])
+    sections = section_split(newspaper_content)
 
