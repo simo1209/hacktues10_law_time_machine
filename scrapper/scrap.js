3261d90fc448f8b963bdabb55dd2eb0b390d6447
VGeorgiev1 <vladdislav.georgiev@gmail.com>
2024-03-14 22:07:23 +0200

    changing law headers and adding command line arguments for scrapper

--- scrapper/scrap.js
+++ scrapper/scrap.js
@@ -1,21 +1,49 @@
 const fetch = require('node-fetch');
 const parse = require('node-html-parser').default
-const fs = require('fs')
+const fs = require('fs');
+const { time } = require('console');
 
+let ids;
+
+const firstId = 100;
+
+try{
+  ids = Number(process.argv[2]);
+} catch(e) {
+  throw new Error('invalid argument, should be a number');
+}
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+let romanMap = {
+  'I': 1,
+  'II': 2,
+  'III': 3,
+  'IV': 4,
+  'V': 5,
+  'VI': 6,
+  'VII': 7,
+  'VIII': 8,
+  'IX': 9,
+  'X': 10,
+  'XI': 11,
+  'XII': 12
+}
 
 if(!fs.existsSync('./laws_changes')) {
   fs.mkdirSync('./laws_changes');
 }
-for(let i = 0; i < 100; i++){
-
-  fetch(`https://www.ciela.net/svobodna-zona-darjaven-vestnik/issue/${7000+i}/published`)
+(async() => {
+for(let i = 0; i < ids; i++){
+  await delay(300)
+  fetch(`https://www.ciela.net/svobodna-zona-darjaven-vestnik/issue/${firstId+i}/published`)
     .then((response) => response.text())
     .then((body) => {
 
       let root = parse(body);
       
       let lis = root.getElementsByTagName('li')
-      //console.log(lis)
+
       let links = [];
       lis.forEach((li) => {
         if(li.text.indexOf('ЗАКОН ЗА ИЗМЕНЕНИЕ') != -1) {
@@ -29,24 +57,33 @@ for(let i = 0; i < 100; i++){
       year = year.substring(0, year.length-7);
 
       let issue = root.getElementsByTagName('h1')[0].text.split(',')[1];
-
-      let meta = year + issue;
+      let [count, date_month] = issue.split('от');
       
-      links.forEach((link, idx) =>
-        fetch('https://www.ciela.net/'+link)
-          .then((response) => response.text())
-          .then((body) => {
-            const root = parse(body)
-            let textEl = root.getElementById('LatestEditionRepealed');
-            if(!textEl) {
-              //console.log(root)
-            } else {
-            let finalText = root.getElementById('LatestEditionRepealed').text
-              fs.writeFile('./laws_changes/'+i+'.'+idx+'.txt', meta +'\n' + finalText, (err) => {
-                if (err) throw err;
-              });
-            }
-          })
+      //console.log(count, date_month);
+      let [date, month] = date_month.split('.')
+
+      let timestamp = new Date(`${year}-${romanMap[month]}-${date}`).toISOString().split('T')[0]
+
+      links.forEach((link, idx) => {
+          let fullUrl = 'https://www.ciela.net/'+link;
+          fetch(fullUrl)
+            .then((response) => response.text())
+            .then((body) => {
+              const root = parse(body)
+              let textEl = root.getElementById('LatestEditionRepealed');
+              if(textEl) {
+                let header = fullUrl + '\n' + count;
+                let finalText = root.getElementById('LatestEditionRepealed').text
+                fs.writeFile(`./laws_changes/${timestamp}.${idx}.txt`, header +'\n' + finalText, (err) => {
+                  if (err) throw err;
+                });
+              }
+            })
+        }
       )   
     })
+    .catch((e) => {
+      console.log(e.errno)
+    })
 }
+})()
